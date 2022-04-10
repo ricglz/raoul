16cb0ee21dbf794ff1fc7a1e1a7381f4aeac92ac
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-04-10 16:36:17 -0500

    Add debug for descriptive messages

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, PartialEq)]
+use core::fmt;
+
+#[derive(PartialEq)]
 pub enum RaoulError {
     Invalid,
     UndeclaredVar { name: String },
@@ -6,3 +8,13 @@ pub enum RaoulError {
 }
 
 pub type Result<T> = std::result::Result<T, RaoulError>;
+
+impl fmt::Debug for RaoulError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            RaoulError::Invalid => unreachable!(),
+            RaoulError::UndeclaredVar { name } => write!(f, "Variable \"{}\" is undeclared", name),
+            RaoulError::UnitializedVar { name } => write!(f, "Variable \"{}\" is undeclared", name),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn main() {
     }
     let mut dir_func = DirFunc::new();
     if let Err(error) = build_dir_func(&mut dir_func, ast) {
-        println!("DirFunc error {:?}", error);
+        println!("{:?}", error);
         exit(1);
     }
     if debug {
