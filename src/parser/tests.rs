7a6bdedea1999c32c59e53f87ddd231eb78d8cc1
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-04-15 14:36:47 -0500

    Add more examples to test

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -2,16 +2,23 @@ use super::*;
 use std::fs::read_dir;
 
 #[test]
-fn example_files() {
-    let paths = read_dir("examples").unwrap();
+fn valid_files() {
+    let paths = read_dir("examples/valid").unwrap();
     for path in paths {
         let file_path = path.expect("File must exist").path();
         let file = file_path.to_str().unwrap();
-        if file == "examples/complete.ra" {
+        if file == "examples/valid/complete.ra" {
             continue;
         }
         let program = std::fs::read_to_string(file).expect(file);
         println!("Testing {:?}", file);
         assert!(parse(&program, true).is_ok());
     }
+}
+
+#[test]
+fn invalid_file() {
+    let filename = "examples/invalid/syntax-error.ra";
+    let program = std::fs::read_to_string(filename).expect(filename);
+    assert!(parse(&program, true).is_err());
 }
