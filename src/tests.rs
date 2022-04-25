cbe19630bc9486d280a74744603d622f94a486a1
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-04-24 20:29:29 -0500

    Add test of function redefinition

--- src/tests.rs
+++ src/tests.rs
@@ -12,3 +12,16 @@ fn use_of_undeclared_variable() {
     let build_res = dir_func.build_dir_func(ast);
     assert!(build_res.is_err());
 }
+
+#[test]
+fn redefinition_of_function() {
+    let filename = "examples/invalid/redefined-function.ra";
+    let program = std::fs::read_to_string(filename).expect(filename);
+    let debug = true;
+    let ast_response = parse(&program, debug);
+    assert!(ast_response.is_ok());
+    let ast = ast_response.unwrap();
+    let mut dir_func = DirFunc::new();
+    let build_res = dir_func.build_dir_func(ast);
+    assert!(build_res.is_err());
+}
