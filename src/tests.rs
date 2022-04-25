7fd6e07a7d3219c5a0ff26acdb0a849dc7ba6d7d
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-04-24 20:49:22 -0500

    Add test and example for InvalidCast error

--- src/tests.rs
+++ src/tests.rs
@@ -38,3 +38,16 @@ fn redefine_variable_type() {
     let build_res = dir_func.build_dir_func(ast);
     assert!(build_res.is_err());
 }
+
+#[test]
+fn invalid_cast() {
+    let filename = "examples/invalid/invalid-cast.ra";
+    let program = std::fs::read_to_string(filename).expect(filename);
+    let debug = true;
+    let ast_response = parse(&program, debug);
+    assert!(ast_response.is_ok());
+    let ast = ast_response.unwrap();
+    let mut dir_func = DirFunc::new();
+    let build_res = dir_func.build_dir_func(ast);
+    assert!(build_res.is_err());
+}
