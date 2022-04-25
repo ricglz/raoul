afc5489bd64e2cd6c7f04ea6d8dd5671892cd792
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-04-24 20:44:01 -0500

    Add test and example for RedefineType

--- src/tests.rs
+++ src/tests.rs
@@ -25,3 +25,16 @@ fn redefinition_of_function() {
     let build_res = dir_func.build_dir_func(ast);
     assert!(build_res.is_err());
 }
+
+#[test]
+fn redefine_variable_type() {
+    let filename = "examples/invalid/redefine-variable-type.ra";
+    let program = std::fs::read_to_string(filename).expect(filename);
+    let debug = true;
+    let ast_response = parse(&program, debug);
+    assert!(ast_response.is_ok());
+    let ast = ast_response.unwrap();
+    let mut dir_func = DirFunc::new();
+    let build_res = dir_func.build_dir_func(ast);
+    assert!(build_res.is_err());
+}
