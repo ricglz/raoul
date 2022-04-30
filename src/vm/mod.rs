fe5e32edad0c473769c0238ca5c3f5589a57e87c
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-04-30 13:20:25 -0500

    Implement read, binary and unary operations in VM

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Ordering, io::stdin};
+
 use crate::{
     address::{ConstantMemory, Memory, TOTAL_SIZE},
     dir_func::{function::Function, variable_value::VariableValue, FunctionTable},
@@ -54,46 +56,46 @@ impl VM {
     }
 
     #[inline]
-    pub fn current_context(&self) -> &VMContext {
+    fn current_context(&self) -> &VMContext {
         self.contexts_stack.last().unwrap()
     }
 
     #[inline]
-    pub fn local_addresses(&self) -> Memory {
+    fn local_addresses(&self) -> Memory {
         self.current_context().local_memory.clone()
     }
 
     #[inline]
-    pub fn temp_addresses(&self) -> Memory {
+    fn temp_addresses(&self) -> Memory {
         self.current_context().temp_memory.clone()
     }
 
     #[inline]
-    pub fn current_context_mut(&mut self) -> &mut VMContext {
+    fn current_context_mut(&mut self) -> &mut VMContext {
         self.contexts_stack.last_mut().unwrap()
     }
 
     #[inline]
-    pub fn local_addresses_mut(&mut self) -> &mut Memory {
+    fn local_addresses_mut(&mut self) -> &mut Memory {
         &mut self.current_context_mut().local_memory
     }
 
     #[inline]
-    pub fn temp_addresses_mut(&mut self) -> &mut Memory {
+    fn temp_addresses_mut(&mut self) -> &mut Memory {
         &mut self.current_context_mut().temp_memory
     }
 
     #[inline]
-    pub fn update_quad_pos(&mut self, quad_pos: usize) {
+    fn update_quad_pos(&mut self, quad_pos: usize) {
         self.current_context_mut().quad_pos = quad_pos;
     }
 
-    pub fn get_current_quad(&self) -> Quadruple {
+    fn get_current_quad(&self) -> Quadruple {
         let quad_pos = self.current_context().quad_pos;
         self.quad_list.get(quad_pos).unwrap().clone()
     }
 
-    pub fn get_value(&self, address: usize) -> VariableValue {
+    fn get_value(&self, address: usize) -> VariableValue {
         match address / TOTAL_SIZE {
             0 => self.global_memory.get(address).unwrap(),
             1 => self.local_addresses().get(address).unwrap(),
@@ -103,7 +105,7 @@ impl VM {
         }
     }
 
-    pub fn write_value(&mut self, value: VariableValue, address: usize) {
+    fn write_value(&mut self, value: VariableValue, address: usize) {
         let memory = match address / TOTAL_SIZE {
             0 => &mut self.global_memory,
             1 => self.local_addresses_mut(),
@@ -113,16 +115,70 @@ impl VM {
         memory.write(address, value);
     }
 
-    pub fn process_assign(&mut self) {
+    fn process_assign(&mut self) {
         let quad = self.get_current_quad();
         let value = self.get_value(quad.op_1.unwrap());
         self.write_value(value, quad.res.unwrap());
     }
 
-    pub fn process_print(&mut self) {
+    fn process_print(&mut self) {
         let quad = self.get_current_quad();
         let value = self.get_value(quad.op_1.unwrap());
         print!("{value:?} ")
+    }
+
+    fn create_value_from_stdin(&self) -> VariableValue {
+        let mut line = String::new();
+        stdin().read_line(&mut line).unwrap();
+        VariableValue::String(line)
+    }
+
+    fn process_read(&mut self) {
+        let quad = self.get_current_quad();
+        let value = self.create_value_from_stdin();
+        self.write_value(value, quad.res.unwrap());
+    }
+
+    fn unary_operation<F>(&mut self, f: F)
+    where
+        F: FnOnce(VariableValue) -> VariableValue,
+    {
+        let quad = self.get_current_quad();
+        let a = self.get_value(quad.op_1.unwrap());
+        let value = f(a);
+        self.write_value(value, quad.res.unwrap());
+    }
+
+    fn binary_operation<F>(&mut self, f: F)
+    where
+        F: FnOnce(VariableValue, VariableValue) -> VariableValue,
+    {
+        let quad = self.get_current_quad();
+        let a = self.get_value(quad.op_1.unwrap());
+        let b = self.get_value(quad.op_2.unwrap());
+        let value = f(a, b);
+        self.write_value(value, quad.res.unwrap());
+    }
+
+    fn comparison(&mut self) {
+        let quad = self.get_current_quad();
+        let a = self.get_value(quad.op_1.unwrap());
+        let b = self.get_value(quad.op_2.unwrap());
+        let ord = a.partial_cmp(&b);
+        let res = match ord {
+            None => false,
+            Some(ord) => match quad.operator {
+                Operator::Lt => ord == Ordering::Less,
+                Operator::Lte => ord != Ordering::Greater,
+                Operator::Gt => ord == Ordering::Greater,
+                Operator::Gte => ord != Ordering::Less,
+                Operator::Eq => ord == Ordering::Equal,
+                Operator::Ne => ord != Ordering::Equal,
+                _ => unreachable!(),
+            },
+        };
+        let value = VariableValue::Bool(res);
+        self.write_value(value, quad.res.unwrap());
     }
 
     pub fn run(&mut self) {
@@ -135,7 +191,21 @@ impl VM {
                 Operator::Assignment => self.process_assign(),
                 Operator::Print => self.process_print(),
                 Operator::PrintNl => println!(""),
-                kind => unreachable!("{:?}", kind),
+                Operator::Read => self.process_read(),
+                Operator::Or => self.binary_operation(|a, b| a | b),
+                Operator::And => self.binary_operation(|a, b| a & b),
+                Operator::Sum => self.binary_operation(|a, b| a + b),
+                Operator::Minus => self.binary_operation(|a, b| a - b),
+                Operator::Times => self.binary_operation(|a, b| a * b),
+                Operator::Div => self.binary_operation(|a, b| a / b),
+                Operator::Lt
+                | Operator::Lte
+                | Operator::Gt
+                | Operator::Gte
+                | Operator::Eq
+                | Operator::Ne => self.comparison(),
+                Operator::Not => self.unary_operation(|a| !a),
+                kind => todo!("{:?}", kind),
             }
             self.update_quad_pos(quad_pos + 1);
         }
